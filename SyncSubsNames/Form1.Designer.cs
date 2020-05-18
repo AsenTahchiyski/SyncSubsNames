4576bed14df8dcf2d6ea2544b94de2663954fc50
AsenTahchiyski <malkstor@gmail.com>
2020-05-18 23:05:04 +0300

    Version 1.0

---
+++ SyncSubsNames/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace SyncSubsNames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectFolder = new System.Windows.Forms.Button();
+            this.OutputText = new System.Windows.Forms.TextBox();
+            this.ProcessInnerFolders = new System.Windows.Forms.CheckBox();
+            this.Rename = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SelectFolder
+            // 
+            this.SelectFolder.Location = new System.Drawing.Point(12, 12);
+            this.SelectFolder.Name = "SelectFolder";
+            this.SelectFolder.Size = new System.Drawing.Size(97, 23);
+            this.SelectFolder.TabIndex = 0;
+            this.SelectFolder.Text = "Select Folder";
+            this.SelectFolder.UseVisualStyleBackColor = true;
+            this.SelectFolder.Click += new System.EventHandler(this.SelectFolder_Click);
+            // 
+            // OutputText
+            // 
+            this.OutputText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputText.Location = new System.Drawing.Point(12, 41);
+            this.OutputText.Multiline = true;
+            this.OutputText.Name = "OutputText";
+            this.OutputText.ReadOnly = true;
+            this.OutputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputText.Size = new System.Drawing.Size(507, 345);
+            this.OutputText.TabIndex = 1;
+            this.OutputText.TextChanged += new System.EventHandler(this.OutputText_TextChanged);
+            // 
+            // ProcessInnerFolders
+            // 
+            this.ProcessInnerFolders.AutoSize = true;
+            this.ProcessInnerFolders.Location = new System.Drawing.Point(126, 18);
+            this.ProcessInnerFolders.Name = "ProcessInnerFolders";
+            this.ProcessInnerFolders.Size = new System.Drawing.Size(124, 17);
+            this.ProcessInnerFolders.TabIndex = 2;
+            this.ProcessInnerFolders.Text = "Process inner folders";
+            this.ProcessInnerFolders.UseVisualStyleBackColor = true;
+            // 
+            // Rename
+            // 
+            this.Rename.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rename.Location = new System.Drawing.Point(444, 12);
+            this.Rename.Name = "Rename";
+            this.Rename.Size = new System.Drawing.Size(75, 23);
+            this.Rename.TabIndex = 3;
+            this.Rename.Text = "Rename";
+            this.Rename.UseVisualStyleBackColor = true;
+            this.Rename.Click += new System.EventHandler(this.Rename_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 398);
+            this.Controls.Add(this.Rename);
+            this.Controls.Add(this.ProcessInnerFolders);
+            this.Controls.Add(this.OutputText);
+            this.Controls.Add(this.SelectFolder);
+            this.Name = "Form1";
+            this.Text = "Sync Subtitles Names";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button SelectFolder;
+        private System.Windows.Forms.TextBox OutputText;
+        private System.Windows.Forms.CheckBox ProcessInnerFolders;
+        private System.Windows.Forms.Button Rename;
+    }
+}
+
---
+++ SyncSubsNames/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace SyncSubsNames
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly string[] VideoExtensions = {
+            "avi", "mkv", "m4v", "ts", "mp4", "mpg", "mpeg", "mp2", "mpe", "mpv", "m4p", "m4v", "wmv", "mov"};
+
+        private static readonly string[] SubsExtensions = { "sub", "srt" };
+
+        private static readonly string RegexPattern = "([sS](?'Season'[\\d]+))[-_\\s\\.]?([Ee](?'Episode'[\\d]+))";
+
+        private static string selectedFolder;
+
+        private void SelectFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        // Get the selected folder
+                        string folder = fbd.SelectedPath;
+                        selectedFolder = folder;
+                        AppendToLog($"Selected folder {folder}.");
+                        //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                    }
+                }
+            } 
+            catch (Exception exception)
+            {
+                AppendToLog($"OH SHI$&!@*&% {exception.ToString()}");
+            }
+        }
+
+        private void Rename_Click(object sender, EventArgs e)
+        {
+            if (ProcessInnerFolders.Checked)
+            {
+                ProcessFolderRecursively(selectedFolder);
+            }
+            else
+            {
+                ProcessFolder(selectedFolder);
+            }
+        }
+
+        private void AppendToLog(string text)
+        {
+            OutputText.Text += text + Environment.NewLine;
+        }
+
+        private void ProcessFolderRecursively(string folder)
+        {
+            ProcessFolder(folder);
+            string[] allSubfolders = Directory.GetDirectories(folder);
+            foreach(string subfolder in allSubfolders)
+            {
+                ProcessFolderRecursively(subfolder);
+            }
+        }
+
+        private void ProcessFolder(string folder)
+        {
+            string[] allFiles = Directory.GetFiles(folder);
+
+            // Get video files
+            var videoFiles = allFiles.Where(file => HasExtension(file, VideoExtensions)).ToArray();
+            AppendToLog($"Found {videoFiles.Length} video files.");
+
+            // Get subtitle files
+            var subsFiles = allFiles.Where(file => HasExtension(file, SubsExtensions)).ToArray();
+            AppendToLog($"Found {subsFiles.Length} subtitle files.");
+
+            // Go through all subtitle files for each video and see if they match
+            int subtitlesRenamed = 0;
+            foreach (string video in videoFiles)
+            {
+                foreach (string subtitle in subsFiles)
+                {
+                    // Rename if necessary
+                    if (IsRightSubtitle(subtitle, video) && RenameSubtitle(subtitle, video))
+                    {
+                        AppendToLog($"Renamed: {Path.GetFileNameWithoutExtension(subtitle)}");
+                        subtitlesRenamed++;
+                        break;
+                    }
+                }
+            }
+
+            AppendToLog($"{subtitlesRenamed} subtitles renamed.");
+            AppendToLog("======================================");
+        }
+
+        
+
+        private static bool HasExtension(string fileName, string[] extensions)
+        {
+            foreach (string extension in extensions)
+            {
+                if (fileName.EndsWith("." + extension))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool IsRightSubtitle(string subtitle, string video)
+        {
+            if (!HasExtension(subtitle, SubsExtensions) || !HasExtension(video, VideoExtensions))
+            {
+                return false;
+            }
+
+            var matchesVideo = Regex.Match(video, RegexPattern);
+            var matchesSubtitle = Regex.Match(subtitle, RegexPattern);
+
+            if (!matchesSubtitle.Success || !matchesVideo.Success)
+            {
+                return false;
+            }
+
+            int videoSeason = GetSeason(matchesVideo.Groups);
+            int videoEpisode = GetEpisode(matchesVideo.Groups);
+
+            int subtitleSeason = GetSeason(matchesSubtitle.Groups);
+            int subtitleEpisode = GetEpisode(matchesSubtitle.Groups);
+
+            // Check if season and episode are same
+            if (videoSeason == subtitleSeason && videoEpisode == subtitleEpisode &&
+                videoSeason >= 0 && videoEpisode >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static int GetEpisode(GroupCollection groups)
+        {
+            foreach (Group group in groups)
+            {
+                if (group.Name == "Episode")
+                {
+                    var value = group.Value;
+                    if (int.TryParse(value, out int episode))
+                    {
+                        return episode;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private static int GetSeason(GroupCollection groups)
+        {
+            foreach (Group group in groups)
+            {
+                if (group.Name == "Season")
+                {
+                    var value = group.Value;
+                    if (int.TryParse(value, out int season))
+                    {
+                        return season;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private static bool RenameSubtitle(string subtitle, string video)
+        {
+            try
+            {
+                string oldName = Path.GetFileName(subtitle);
+                string newName = Path.GetFileNameWithoutExtension(video) + ".srt";
+                string newPath = subtitle.Replace(oldName, newName);
+                File.Move(subtitle, newPath);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void OutputText_TextChanged(object sender, EventArgs e) { }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        //    private void Rename_Click(object sender, EventArgs e)
+        //    {
+
+        //    }
+    }
+}
